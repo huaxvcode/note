23229c2db301a1df3074b2a350fe6a545dbad586
huaxvcode <huaxvcode@yeah.net>
2024-02-20 04:13:21 +0800

    js 实现网格

--- HTML/24.div 实现网格/static/js/index.js
+++ HTML/24.div 实现网格/static/js/index.js
@@ -3,18 +3,27 @@ let n = 100;
 
 let getColumn = function(x, y) {
     let s = `<div class="column r${x} c${y}"></div>`;
+    return s;
 };
 
 let getRow = function(x) {
     let s = `<div class="row r${x}">`;
     for (let i = 1; i <= n; i ++) {
-
+        s += getColumn(x, i);
     }
     s += "</div>";
+    return s;
 };
 
+let getGrid = () => {
+    let grid = document.querySelector(".grid");
+    for (let i = 1; i <= n; i ++) {
+        grid.innerHTML += getRow(i);
+    }
+}
+
 let debug = () => {
-    
+    getGrid();
 };
 
 let main = () => {
